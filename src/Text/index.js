b7b4cc6844f285becfd73bc2f5d02b5dffec3b07
Felix Volny <felix@volny.co>
2018-04-09 16:32:26 +0700

    barebones Text component

---
+++ src/Text/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './index.css'
+import typography from '../typography.css'
+
+const Text = ({
+  text,
+  align = 'left',
+  bold = false,
+  inline = false,
+  italic = false,
+  size = 'md',
+}) => {
+  // const cs = cx(
+  //   styles.Text,
+  //   align === 'center' && typography.alignCenter,
+  //   align === 'justify' && typography.alignJustify,
+  //   align === 'left' && typography.alignLeft,
+  //   align === 'right' && typography.alignRight,
+  //   italic && typography.fontStyleItalic,
+  //   !italic && typography.fontStyleNormal,
+  //   bold && typography.fontWeightBold,
+  //   !bold && typography.fontWeightNormal,
+  // );
+
+  const Tag = inline ? 'span' : 'div'
+
+  // return (
+  //   <Tag className={cs} >
+  //     {text}
+  //   </Tag>
+  // );
+
+  return <Tag>{text}</Tag>
+}
+
+Text.propTypes = {
+  text: PropTypes.string,
+  align: PropTypes.oneOf(['left', 'right', 'center', 'justify']),
+  bold: PropTypes.bool,
+  inline: PropTypes.bool,
+  italic: PropTypes.bool,
+  size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+}
+
+export default Text
