ff18a3a75b5194322433da01131edeedeb3d2209
Felix Volny <felix@volny.co>
2018-04-09 15:13:01 +0700

    remove github link to component - todo later

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -8,9 +8,11 @@ type Props = {|
   description: string,
 |};
 
+// TODO
 const githubSourceUrl = (component: string): string => {
-  const file = component.replace(/\s+/g, '');
-  return `https://github.com/pinterest/gestalt/blob/master/packages/gestalt/src/${file}/${file}.js`;
+  // const file = component.replace(/\s+/g, '');
+  // return `https://github.com/pinterest/gestalt/blob/master/packages/gestalt/src/${file}/${file}.js`;
+  return `https://github.com/Diakrit`
 };
 
 export default function PageHeader({ name, description = '' }: Props) {
