bb87272ab82b5ce063b9e1389714f0eb6551214f
Felix Volny <felix@volny.co>
2018-04-10 14:28:52 +0700

    dependencies - prop-types, webpack, classnames

--- src/Text/Text.js
+++ src/Text/Text.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
+
 import styles from './index.css';
 import typography from '../typography.css';
 import colors from '../Colors.css';
