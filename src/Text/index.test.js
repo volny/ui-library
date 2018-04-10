30948a82b78e3b120233a518ede97af49e679b34
Felix Volny <felix@volny.co>
2018-04-10 11:42:11 +0700

    sample test file

---
+++ src/Text/index.test.js
@@ -0,0 +1,34 @@
+/* eslint-env jest */
+import React from 'react';
+import { create } from 'react-test-renderer';
+import Text from '.';
+
+test('Text renders', () => {
+  const tree = create(<Text />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Text orange adds the orange color class', () => {
+  const tree = create(<Text color="orange" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Text truncate should add a title when the children are text only', () => {
+  const tree = create(
+    <Text truncate>
+      Shall I compare thee to a summer&#39;s day - William Shakespeare
+    </Text>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Text truncate should not add a title when the children are objects', () => {
+  const tree = create(
+    <Text truncate>
+      <div>Summer reading:</div>
+      Shall I compare thee to a summer&#39;s day - William Shakespeare
+    </Text>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
