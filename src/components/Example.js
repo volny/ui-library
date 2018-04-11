db32035421cf7d35c0096476616d07f3e800c2a3
Felix Volny <felix@volny.co>
2018-04-11 09:06:59 +0700

    put ui-library in scope for code examples

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import * as gestalt from 'gestalt';
+import * as uiLibrary from 'ui-library'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import Card from './Card';
 import Checkerboard from './Checkerboard';
@@ -28,6 +29,7 @@ export default ({
       code={defaultCode.trim()}
       scope={{
         ...gestalt,
+        ...uiLibrary,
         ...scope,
       }}
     >
