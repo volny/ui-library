df57b2a9d0fb283e4d4124a93b50b58a9567cc2e
Felix Volny <felix@volny.co>
2018-04-12 15:01:31 +0700

    colors.js

---
+++ src/colors.js
@@ -0,0 +1,23 @@
+// color overrides {
+//  accentColor: '#080',
+//  primaryColor: '#fff',
+//  secondaryColor: '#000',
+// }
+
+export default {
+  /* primary colors */
+  darkGrey: '#373232',
+  white: '#fff',
+  green: '#080',
+
+  /* secondary colors */
+  darkGrey70: 'rgba(50,50,50,0.7)',
+  darkGrey30: 'rgba(50,50,50,0.3)',
+  darkGrey10: 'rgba(50,50,50,0.1)',
+  white90: 'rgba(255,255,255,0.9)',
+  white30: 'rgba(255,255,255,0.3)',
+  green30: 'rgba(0,136,0,0.3)',
+  green20: 'rgba(0,136,0,0.2)',
+  yellow: '#f8d51c',
+  red: '#bf5353',
+}
