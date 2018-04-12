244b83810fafffb2b6823e23f268af6e800c10db
Felix Volny <felix@volny.co>
2018-04-12 08:26:07 +0700

    switch to classic babel presets

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['es2015', 'stage-1', 'react']
           }
         }
       },
