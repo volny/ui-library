8482c10c31178aa9520f786dc6f8911127e48ace
Felix Volny <felix@volny.co>
2018-04-16 16:46:41 +0700

    complete prop docs for Heading

--- docs/src/Heading.doc.js
+++ docs/src/Heading.doc.js
@@ -28,6 +28,16 @@ card(
         type: 'React.Node',
         required: true,
       },
+      {
+        name: 'color',
+        type: `'darkGrey' | 'white' | 'green' | 'darkGrey70' | 'darkGrey30 | 'darkGrey10' | 'white90' | 'white30' | 'green30' | 'green20' | 'yellow' | 'red'`,
+        defaultValue: 'darkGray',
+      },
+      {
+        name: 'level',
+        type: `'1' | '2' | '3' | '4' | '5' `,
+        defaultValue: '1',
+      },
     ]}
     heading={false}
   />
@@ -35,8 +45,8 @@ card(
 
 card(
   <Example
-    description="If you want your link to be text you should wrap a Link component inside of a Text component."
-    name="Text styling"
+    description="Level hierarchy will create an h1 - h5 tag with corresponding styling"
+    name="Levels"
     defaultCode={`
 <Box>
   <Heading level="1">Some Heading</Heading>
@@ -47,6 +57,21 @@ card(
 </Box>
 `}
     scope={{ Box, Heading }}
+  />
+)
+
+card(
+  <Example
+    description="
+    You can specify which color you want for your heading
+  "
+    name="Colors"
+    defaultCode={`
+<Box>
+  <Heading level="2" color="darkGray">Dark Gray (default)</Heading>
+  <Heading level="4" color="green">Diakrit Green</Heading>
+  <Heading color="darkGrey70">Dark Grey Alpha 70</Heading>
+</Box>`}
   />
 )
 
