2c68621ffe95d6d350efac19d14449fdaedda9d8
Felix Volny <felix@volny.co>
2018-04-18 15:26:58 +0700

    complete Button primary styles

--- docs/src/Button.doc.js
+++ docs/src/Button.doc.js
@@ -10,11 +10,16 @@ import { Button } from 'lib'
 const cards = []
 const card = c => cards.push(c)
 
-card(<PageHeader name="Button" description="It's a " />)
+card(<PageHeader name="Button" description="Work in progress" />)
 
 card(
   <PropTable
     props={[
+      {
+        name: 'active',
+        type: 'boolean',
+        defaultValue: false,
+      },
       {
         name: 'disabled',
         type: 'boolean',
@@ -46,81 +51,18 @@ card(
 
 card(
   <Example
-    description="It's the Primary Button"
+    description="Primary Button"
     name="Primary Button"
     defaultCode={`
 <Box maxWidth="8em">
   <Button text="Click Me" />
-  <Button text="Click Me" disabled />
-  <Button text="Click Me" inline />
-  <Button text="Click Me" inline />
+  <Button text="I'm disabled" disabled />
+  <Button text="I'm active" active />
+  <Button text="I'm inline" inline />
+  <Button text="I'm inline" inline />
   <Button text="Submit Me" type="submit" />
 </Box>`}
     scope={{ Box, Button }}
-  />
-)
-
-card(
-  <Example
-    description={`
-    The Text component allows you to specifiy whether you want \`block\` or \`inline\` text.
-  `}
-    name="Block vs inline"
-    defaultCode={`
-<Box>
-  <Text>Some content in a default block element. (default)</Text>
-  <Text inline>Inline text with the inline prop.</Text>
-  {' '}
-  <Text inline>More inline text.</Text>
-</Box>`}
-  />
-)
-
-card(
-  <Example
-    description="
-    You can specify which color you want for your text.
-  "
-    name="Colors"
-    defaultCode={`
-<Box>
-  <Text color="darkGray">Dark Gray (default)</Text>
-  <Text color="green">Diakrit Green</Text>
-  <Text color="darkGrey70">Dark Grey Alpha 70</Text>
-  <Box color="darkGray" maxWidth="8em">
-    <Text color="white">White</Text>
-  </Box>
-</Box>`}
-  />
-)
-
-card(
-  <Example
-    description="
-    Text can be styled as a <code>caption</code> for subscripts and annotations.
-  "
-    name="Caption"
-    defaultCode={`
-<Box>
-  <Text>Default Text</Text>
-  <Text caption>Text Caption</Text>
-</Box>`}
-  />
-)
-
-card(
-  <Example
-    description="
-    There are multiple styles such as bold and italic that we can
-    attach to the Text component.
-  "
-    name="Styles"
-    defaultCode={`
-<Box>
-  <Text bold>Bold</Text>
-  <Text italic>Italic</Text>
-  <Text italic bold caption color="yellow">Heavily styled Text</Text>
-</Box>`}
   />
 )
 
